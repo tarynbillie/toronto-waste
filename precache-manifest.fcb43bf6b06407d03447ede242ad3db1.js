ad88577814877f42887138d135d00b1020ffc68f
Taryn Li <tarynli@hotmail.com>
2019-01-11 14:57:47 -0500

    Updates

---
+++ precache-manifest.fcb43bf6b06407d03447ede242ad3db1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/toronto-waste/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b5945f65cd01d2dfa1dd",
+    "url": "/toronto-waste/static/js/runtime~main.b5945f65.js"
+  },
+  {
+    "revision": "050362b97445fe2b6c73",
+    "url": "/toronto-waste/static/js/main.050362b9.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/toronto-waste/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "050362b97445fe2b6c73",
+    "url": "/toronto-waste/static/css/main.ede19236.chunk.css"
+  },
+  {
+    "revision": "7991868f2b9426f51279f929a5c8cb40",
+    "url": "/toronto-waste/index.html"
+  }
+];
